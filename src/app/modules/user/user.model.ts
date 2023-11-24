5798c500088ffa5ddf7b02c631301396a4c82dc1
Mohammed Akhtar <watchonlytutorial@gmail.com>
2023-11-24 23:28:55 +0600

    User model created

--- src/app/modules/user/user.model.ts
+++ src/app/modules/user/user.model.ts
@@ -1,7 +1,13 @@
-import { Schema } from "mongoose";
-import { IAddress, IUser, UserModel } from "./user.interface";
+import { Schema, model } from "mongoose";
+import {
+  IAddress,
+  IFullName,
+  IOrder,
+  IUser,
+  UserModel,
+} from "./user.interface";
 
-const fullNameSchema = new Schema<IUser>({
+const fullNameSchema = new Schema<IFullName>({
   firstName: {
     type: String,
     required: [true, "First Name is required"],
@@ -10,6 +16,12 @@ const fullNameSchema = new Schema<IUser>({
     type: String,
     readonly: [true, "Last Name is required"],
   },
+});
+
+const orderSchema = new Schema<IOrder>({
+  productName: { type: String, required: true },
+  price: { type: Number, required: true },
+  quantity: { type: Number, required: true },
 });
 
 const addressSchema = new Schema<IAddress>({
@@ -29,4 +41,49 @@ const UserSchema = new Schema<IUser, UserModel>({
     required: [true, "Full Name is required"],
     trim: true,
   },
+  userName: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: [true, "Password is required"],
+  },
+  age: {
+    type: Number,
+    required: [true, "Age is required"],
+  },
+  email: {
+    type: String,
+    required: [true, "Email is required"],
+    unique: true,
+  },
+  isActive: {
+    type: String,
+    enum: ["Active", "Blocked"],
+    default: "Active",
+  },
+  hobbies: {
+    type: [String],
+  },
+  address: {
+    type: addressSchema,
+    required: [true, "Address is required"],
+  },
+  orders: {
+    type: [orderSchema],
+  },
+  isDeleted: {
+    type: Boolean,
+    default: false,
+  },
 });
+
+UserSchema.statics.userExists = async (userId: string) => {
+  const existingUser = await User.findOne({ userId });
+  return existingUser;
+};
+
+// create model:
+export const User = model<IUser, UserModel>("User", UserSchema);
